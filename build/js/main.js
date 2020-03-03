2bdc892443cf448ea26c8387425e9fbf23e5ad57
Rodion <bro171194@gmail.com>
2020-03-03 16:52:02 +0300

    Improved performance. Added the "my skills" block

--- build/js/main.js
+++ build/js/main.js
@@ -1,42 +1,37 @@
-let bcg = document.querySelector('.bcg');
-let photo = document.querySelector('.photo');
-let hello = document.querySelector('.hello');
-let leftSidebar = document.querySelector('.left-sidebar');
-let aboutMe = document.querySelector('.about-me')
-let divs = aboutMe.querySelectorAll('div');
-var divCounter = 0;
+let bcg = document.querySelector('.bcg'),
+    photo = document.querySelector('.photo'),
+    hello = document.querySelector('.hello'),
+    leftSidebar = document.querySelector('.left-sidebar'),
+    aboutMe = document.querySelector('.about-me'),
+    divs = aboutMe.querySelectorAll('div');
+let divCounter = 0;
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(()=>{
-        bcg.classList.remove('invisible');
-        hello.classList.remove('invisible');
-
-        hello.classList.add('slide-in-blurred-right');
+    // setTimeout(()=>{
+        hello.className = 'hello slide-in-blurred-right';
         bcg.style.opacity = '1';
         leftSidebar.style.opacity = '1';
-    }, 0)
+    // }, 0)
     
-    setTimeout(()=>{
-        photo.classList.remove('invisible');
-        photo.classList.add('slide-in-blurred-left');
-    }, 500)
+    // setTimeout(()=>{
+        photo.className = 'photo slide-in-blurred-left';
+    // }, 1000)
 
     setTimeout(()=>{
-        bcg.classList.add('motion');
+        bcg.className = "bcg motion";
         setInterval(()=>{
             bcg.classList.contains('motion') ? bcg.classList.remove('motion') : bcg.classList.add('motion');
         }, 50000)
-    }, 2000)
+    }, 3000)
 });
 
 let slowShowDiv = function() {
-    console.log('fucn')
     if (divCounter >= divs.length-1)
         clearInterval(divTimer)
-    divs[divCounter].classList.add('on-own-position');
+    divs[divCounter].className += ' on-own-position';
     divCounter++
 }
 
-var divTimer = setInterval(slowShowDiv, 1000);
+let divTimer = setInterval(slowShowDiv, 1000);
 
